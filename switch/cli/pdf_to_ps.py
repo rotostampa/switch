90f9d4f565f1b85f681eb9a41994a59796ae3b5b
riccardodivirgilio <riccardodivirgilio@gmail.com>
2024-07-31 13:20:14 +0200

    add output dir

--- switch/cli/pdf_to_ps.py
+++ switch/cli/pdf_to_ps.py
@@ -10,9 +10,11 @@ from switch.utils.uuid import uuid7
 
 @click.command(help="Convert pdf to postscript using applescript")
 @click.argument("files", nargs=-1, type=click.Path())
+@click.option(
+    "--output", help="Directory where the file should go, defaults to input directory"
+)
 @click.option("--unique", is_flag=True, help="Add a unique prefix to the files")
 @click.option("--copy", is_flag=True, help="Copy the file instead of moving it")
-@click.option("--output", is_flag=True, help="Copy the file instead of moving it")
 def pdf_to_ps(files, unique, copy, output):
     raise NotImplementedError
 
