bc26003c5f059e4d929452f09e6cf7d5659d63b0
riccardodivirgilio <riccardodivirgilio@gmail.com>
2024-07-31 12:55:49 +0200

    code lint

--- switch/cli/__init__.py
+++ switch/cli/__init__.py
@@ -1,20 +1,5 @@
-import http.client
-import os
-import pathlib
-import shutil
-import ssl
-import subprocess
-import sys
-import tempfile
-import time
-import urllib.parse
-import uuid
-from urllib.parse import urlparse
-
 import click
-from pyftpdlib.authorizers import DummyAuthorizer
-from pyftpdlib.handlers import FTPHandler
-from pyftpdlib.servers import FTPServer
+
 from switch.cli.ftpserver import ftpserver
 from switch.cli.upload import upload
 
