4e935a91de9e043ee7029b5240d855a59cbf5e42
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-05-22 11:36:31 +0200

    go back to pyproject.toml

--- run.sh
+++ run.sh
@@ -2,12 +2,7 @@
 
 # export PYTHONPYCACHEPREFIX="${TMPDIR:-/tmp/}rotostampa-pycache"
 
-uv run                                            \
-    --directory "$(dirname "${BASH_SOURCE[0]}")"  \
-    --isolated --exact --no-env-file --no-project \
-    --link-mode hardlink --no-config              \
-    --with-requirements requirements.txt          \
-    --with-requirements requirements-dev.txt      \
-    --python 3.13.3                               \
-    --python-preference only-managed              \
-    --module switch "$@"
+cd $(dirname "${BASH_SOURCE[0]}")
+
+uv pip install .
+.venv/bin/python -m switch "$@"
