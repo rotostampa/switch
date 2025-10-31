2d8cffcd265e2a71dd2f897bb88c73f47b0597b5
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-10-31 15:12:08 +0100

    add checksum alg

--- switch/cli/upload.py
+++ switch/cli/upload.py
@@ -22,7 +22,7 @@ def upload(files, unique, s3, notify, copy):
     processes = tuple(
         grab_and_run(
             file,
-            lambda path, temp, task_id, s3=s3: (AWSCLI, "s3", "cp", path, s3),
+            lambda path, temp, task_id, s3=s3: (AWSCLI, "s3", "cp", path, s3, "--checksum-algorithm", "SHA256"),
             task_name="switch_file_upload",
             unique=unique,
             copy=copy,
