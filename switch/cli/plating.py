02c8e5e2f13cfe7b9e2f4060b05761f99301526b
riccardodivirgilio <riccardodivirgilio@gmail.com>
2026-03-12 08:40:22 +0100

    add plating

--- switch/cli/plating.py
+++ switch/cli/plating.py
@@ -15,8 +15,14 @@ from switch.utils.run import file_to_temp_dir, grab_and_run, run, open_folder
 @click.option("--unique", is_flag=True, help="Add a unique prefix to the files")
 @click.option("--copy", is_flag=True, help="Copy the file instead of moving it")
 @click.option("--pages", help="Page range to process, e.g. 1-4")
+@click.option("--lpi", default=200, type=int, help="Screen frequency in lines per inch (default: 200)")
+@click.option("--angle-c", default=165, type=int, help="Cyan screen angle (default: 165)")
+@click.option("--angle-m", default=100, type=int, help="Magenta screen angle (default: 100)")
+@click.option("--angle-y", default=0, type=int, help="Yellow screen angle (default: 0)")
+@click.option("--angle-k", default=45, type=int, help="Black screen angle (default: 45)")
+@click.option("--dpi", default=2400, type=int, help="Output resolution in DPI (default: 2400)")
 @click.option("--open", "open_out", is_flag=True, help="Open the output folder when done")
-def plating(files, output, unique, copy, pages, open_out):
+def plating(files, output, unique, copy, pages, lpi, angle_c, angle_m, angle_y, angle_k, dpi, open_out):
     for file in expand_files(*files):
         # Split PDF into single-page files using qpdf
         split_args = ("--pages", ".", pages, "--") if pages else ()
@@ -48,6 +54,20 @@ def plating(files, output, unique, copy, pages, open_out):
         processes = []
         for page_pdf in page_pdfs:
             stem = os.path.splitext(os.path.basename(page_pdf))[0]
+            halftone = (
+                "<< /HalftoneType 5"
+                " /Cyan << /HalftoneType 1 /Frequency {lpi} /Angle {ac}"
+                " /SpotFunction {{dup mul exch dup mul add 1 exch sub}} >>"
+                " /Magenta << /HalftoneType 1 /Frequency {lpi} /Angle {am}"
+                " /SpotFunction {{dup mul exch dup mul add 1 exch sub}} >>"
+                " /Yellow << /HalftoneType 1 /Frequency {lpi} /Angle {ay}"
+                " /SpotFunction {{dup mul exch dup mul add 1 exch sub}} >>"
+                " /Black << /HalftoneType 1 /Frequency {lpi} /Angle {ak}"
+                " /SpotFunction {{dup mul exch dup mul add 1 exch sub}} >>"
+                " /Default << /HalftoneType 1 /Frequency {lpi} /Angle {ak}"
+                " /SpotFunction {{dup mul exch dup mul add 1 exch sub}} >>"
+                " >> sethalftone"
+            ).format(lpi=lpi, ac=angle_c, am=angle_m, ay=angle_y, ak=angle_k)
             p = run((
                 GS,
                 "-q",
@@ -56,9 +76,10 @@ def plating(files, output, unique, copy, pages, open_out):
                 "-dSAFER",
                 "-sDEVICE=tiffsep1",
                 "-sCompression=g4",
-                "-r2400",
+                "-r{}".format(dpi),
+                "-c", halftone,
                 "-sOutputFile=" + os.path.join(out_dir, "{}.tif".format(stem)),
-                page_pdf,
+                "-f", page_pdf,
             ), wait_for_result=False)
             processes.append(p)
 
